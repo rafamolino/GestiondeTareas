d85138b242d75776fa80dbb9dc486faf5f63d4c7
rafamolino <35599091+rafamolino@users.noreply.github.com>
2023-03-29 21:43:36 +0200

    Primera pantala - Inicio de sesión/Registro

---
+++ app/src/main/java/com/example/gestindetareas/LoginActivity.java
@@ -0,0 +1,19 @@
+package com.example.gestindetareas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView btn=findViewById(R.id.textViewSignUp);
+
+    }
+}
